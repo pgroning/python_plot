64cc951012bda14950527b37423358a88fbbf1e6
Per Gröningsson <per.groningsson@vattenfall.com>
2017-03-17 15:04:18 +0100

    hide toolbar

--- qtplot.py
+++ qtplot.py
@@ -4,8 +4,13 @@ mpl.use("qt4Agg")  # Use Qt4 graphics library
 import matplotlib.pyplot as plt
 import numpy as np
 
+from pylab import get_current_fig_manager as gcfm
+
 def plot():
     
+    fig=gcfm()
+    fig.toolbar.hide()
+
     mpl.rcParams["lines.linewidth"] = 2
 
     x = np.linspace(-10,10,100)
@@ -14,7 +19,6 @@ def plot():
     plt.plot(x, y)
     plt.ylabel("sin(x)/x")
     plt.show()
-
 
 if __name__ == "__main__":
     plot()
